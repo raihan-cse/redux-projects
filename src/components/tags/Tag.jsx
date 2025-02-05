ccb67925c3898a1b9e777641060bf3c1bedd798b
raihan-cct <raihan@cocreatetech.com>
2025-02-05 12:02:13 +0600

    intigrate tags data to ui

--- src/components/tags/Tag.jsx
+++ src/components/tags/Tag.jsx
@@ -1,13 +1,14 @@
-export default function Tag() {
-    return (
-        <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">
-            react
-        </div>
-    );
+/* eslint-disable react/prop-types */
+export default function Tag({ title }) {
+  return (
+    <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">
+      {title}
+    </div>
+  );
 }
 
 {
-    /* <div className="bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer">
+  /* <div className="bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer">
 redux
 </div> */
 }
