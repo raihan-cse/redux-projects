2fcec9b8bfb68eda67b5f2186fd5c383fdc41b8a
raihan-cct <raihan@cocreatetech.com>
2025-02-10 05:13:11 +0600

    relatedVideos feature > Slice, API & UI Intigration

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
+import relatedVideosReducer from "../features/relatedVideos/relatedVideosSlice";
 import tagsReducer from "../features/tags/tagsSlice";
 import videoReducer from "../features/video/videoSlice";
 import videosReducer from "../features/videos/videosSlice";
@@ -10,5 +11,6 @@ export const store = configureStore({
     videos: videosReducer,
     tags: tagsReducer,
     video: videoReducer,
+    relatedVideos: relatedVideosReducer,
   },
 });
--- src/components/list/RelatedVideoList.jsx
+++ src/components/list/RelatedVideoList.jsx
@@ -1,9 +1,41 @@
+/* eslint-disable react/prop-types */
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Loading from "../../components/ui/Loading";
+import { fetchRelatedVideos } from "../../features/relatedVideos/relatedVideosSlice";
 import RelatedVideoListItem from "./RelatedVideoListItem";
 
-export default function RelatedVideoList() {
+export default function RelatedVideoList({ currentVideoId, tags }) {
+  const dispatch = useDispatch();
+  const { relatedVideos, isLoading, isError, error } = useSelector(
+    (state) => state.relatedVideos
+  );
+
+  useEffect(() => {
+    dispatch(fetchRelatedVideos({ id: currentVideoId, tags }));
+  }, [dispatch, currentVideoId, tags]);
+
+  //deside what to render
+  let content = null;
+
+  if (isLoading) content = <Loading />;
+
+  if (!isLoading && isError)
+    content = <div className="col-span-12">{error}</div>;
+
+  if (!isLoading && !isError && relatedVideos?.length === 0) {
+    content = <div className="col-span-12">No videos found!</div>;
+  }
+
+  if (!isLoading && !isError && relatedVideos?.length > 0) {
+    content = relatedVideos.map((video) => (
+      <RelatedVideoListItem key={video.id} video={video} />
+    ));
+  }
+
   return (
     <div className="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
-      <RelatedVideoListItem />
+      {content}
     </div>
   );
 }
