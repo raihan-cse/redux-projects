71d3638e73a0323297978bc6e3a92cd31d9f9f96
raihan-cct <raihan@cocreatetech.com>
2025-02-10 07:00:09 +0600

    filter & search feature > filterSlice & tag select unselcet intigration

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
+import filterReducer from "../features/filter/filterSlice";
 import relatedVideosReducer from "../features/relatedVideos/relatedVideosSlice";
 import tagsReducer from "../features/tags/tagsSlice";
 import videoReducer from "../features/video/videoSlice";
@@ -12,5 +13,6 @@ export const store = configureStore({
     tags: tagsReducer,
     video: videoReducer,
     relatedVideos: relatedVideosReducer,
+    filter: filterReducer,
   },
 });
--- src/components/tags/Tag.jsx
+++ src/components/tags/Tag.jsx
@@ -1,14 +1,34 @@
 /* eslint-disable react/prop-types */
+import { useDispatch, useSelector } from "react-redux";
+import { tagRemoved, tagSelected } from "../../features/filter/filterSlice";
+
 export default function Tag({ title }) {
+  const dispatch = useDispatch();
+  const { tags: selectedTags } = useSelector((state) => state.filter);
+
+  const isSelected = selectedTags.includes(title) ? true : false;
+
+  const handleToggleSelection = () => {
+    if (isSelected) {
+      dispatch(tagRemoved(title));
+    } else {
+      dispatch(tagSelected(title));
+    }
+  };
+
+  const style = isSelected
+    ? "bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer"
+    : "bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer";
+
   return (
-    <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">
+    <div className={style} onClick={handleToggleSelection}>
       {title}
     </div>
   );
 }
 
 {
-  /* <div className="bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer">
+  /* <div className="">
 redux
 </div> */
 }
