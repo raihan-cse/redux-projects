a3b172f99018b108f5724172b3d4edebe971c52a
raihan-cse <raihancse46@gmail.com>
2025-02-20 03:47:31 +0600

    layout desing with seperated components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import Balance from "./components/Balance";
+import Form from "./components/Form";
+import Layout from "./components/Layout";
+import Transactions from "./components/transactions/Transactions";
 
 function App() {
-  return <h1>Hello Expense</h1>;
+  return (
+    <Layout>
+      <Balance />
+      <Form />
+      <Transactions />
+    </Layout>
+  );
 }
 
 export default App;
---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+export default function Layout({ children }) {
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Expense Tracker</h1>
+      </div>
+      <div className="main">
+        <div className="container">{children}</div>
+      </div>
+      <div className="footer">&copy; Raihan Kabir</div>
+    </div>
+  );
+}
